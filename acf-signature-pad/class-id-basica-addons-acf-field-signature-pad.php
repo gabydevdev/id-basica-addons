faeacbc2e74c8d43dae2410773a7a7ecf9787cdc
Gabriela F. <1626284+gabydevdev@users.noreply.github.com>
2025-06-23 21:11:59 -0700

    refactor(signature-pad): :recycle: migrate to custom JS and CSS, update integration
    * Replaces bundled third-party signature pad assets with a custom, vendor-free implementation
    * Updates field markup and class names for improved maintainability and styling flexibility
    * Switches from legacy selectors and logic to a more modular and ACF-friendly approach
    * Refactors WordPress integration to use the new JS/CSS and script handles
    * Motivated by the need for better extensibility, maintainability, and reduced vendor lock-in

--- acf-signature-pad/class-id-basica-addons-acf-field-signature-pad.php
+++ acf-signature-pad/class-id-basica-addons-acf-field-signature-pad.php
@@ -17,11 +17,11 @@ class id_basica_addons_acf_field_signature_pad extends \acf_field {
 		);
 
 		$this->env = array(
-			'url'     => site_url( str_replace( ABSPATH, '', __DIR__ ) ),
+			'url'     => ID_BASICA_ADDONS_URL . 'acf-signature-pad',
 			'version' => '1.0.0',
 		);
 
-		$this->preview_image = $this->env['url'] . '/assets/images/field-preview-custom.png';
+		$this->preview_image = $this->env['url'] . 'assets/images/field-preview-custom.png';
 
 		parent::__construct();
 	}
@@ -31,15 +31,35 @@ class id_basica_addons_acf_field_signature_pad extends \acf_field {
 	}
 
 	public function render_field( $field ) {
-		$value      = esc_attr( $field['value'] );
-		$field_id   = esc_attr( $field['id'] );
-		$field_name = esc_attr( $field['name'] );
+		/*
+		 *  Review the data of $field.
+		 *  This will show what data is available
+		 */
 
-		echo '<div class="acf-signature-wrapper">';
-		echo '<canvas class="acf-signature-canvas" width="400" height="200"></canvas>';
-		echo '<input type="hidden" name="' . $field_name . '" value="' . $value . '" class="acf-signature-input" />';
-		echo '<button type="button" class="acf-signature-clear button">' . __( 'Clear', 'id-basica-addons' ) . '</button>';
-		echo '</div>';
+		// echo '<pre>';
+		// 	print_r( $field );
+		// echo '</pre>';
+
+
+		/*
+		 *  Create a simple text input using the 'font_size' setting.
+		 */
+
+		?>
+		<div class="acf-input-wrap">
+            <div id="signature-pad" class="m-signature-pad">
+                <input type="hidden" name="<?php echo $field['name']; ?>" value="<?php echo $field['value']; ?>" />
+                <div class="m-signature-pad--body">
+                    <canvas></canvas>
+                </div>
+                <div class="m-signature-pad--footer">
+                    <a href="#clear" class="m-signature-pad--clear btn btn-default btn-xs button button-small" data-action="clear">
+						<?php _e('Clear', 'id-basica-addons'); ?>
+					</a>
+                </div>
+            </div>
+        </div>
+		<?php
 	}
 
 	public function input_admin_enqueue_scripts() {
@@ -47,22 +67,22 @@ class id_basica_addons_acf_field_signature_pad extends \acf_field {
 		$version = $this->env['version'];
 
 		wp_register_script(
-			'id-basica-addons-signature-pad',
-			"{$url}assets/js/acf-signature.js",
-			array( 'acf-input', 'id-basica-addons-signature-pad-lib' ),
-			$version,
-			true
+			'id-basica-addons-signature-pad-lib',
+			"{$url}assets/js/signature_pad.js"
 		);
+		wp_enqueue_script( 'id-basica-addons-signature-pad-lib' );
+
+		wp_register_script(
+			'id-basica-addons-signature-pad',
+			"{$url}assets/js/field.js",
+			array( 'acf-input', 'id-basica-addons-signature-pad-lib' )
+		);
+		wp_enqueue_script( 'id-basica-addons-signature-pad' );
 
 		wp_register_style(
 			'id-basica-addons-signature-pad',
-			"{$url}assets/css/acf-signature.css",
-			array( 'acf-input' ),
-			$version
+			"{$url}assets/css/field.css"
 		);
-
-		wp_enqueue_script( 'id-basica-addons-signature-pad-lib' );
-		wp_enqueue_script( 'id-basica-addons-signature-pad' );
 		wp_enqueue_style( 'id-basica-addons-signature-pad' );
 	}
 }
